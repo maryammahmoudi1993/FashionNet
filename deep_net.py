e6b767bcc43805c23d2fa12b0e4a83462c91cfca
Maryam Mahmoudi <mahmoodi.maryam1993@gmail.com>
2023-01-17 22:53:29 +0330

    train test split finished

--- deep_net.py
+++ deep_net.py
@@ -1,5 +1,10 @@
 import tensorflow as tf  
 from keras import layers, models
+from main import load_data
+
+path = "D:\Python_Codes\Deep_Learning\Advanced Tensorflow\Multi_Label Classification\FashionNet\Week2_dataset\clothes_dataset\dataset"
+load_data(path=path)    
+
 
 class FashionNet():
     
